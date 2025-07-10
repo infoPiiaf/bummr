777ddc5eff1e7295e3009fad0e17d2ee84164e71
Alexandre Friquet <framagit_alex@infopiiaf.fr>
2025-07-10 10:57:10 +0200

    Add test within included forlder.

--- spec/black_box/bummr_update_spec.rb
+++ spec/black_box/bummr_update_spec.rb
@@ -1,29 +1,29 @@
 require "spec_helper"
 require "jet_black"
 
-describe "bummr update command" do
-  let(:session) { JetBlack::Session.new(options: { clean_bundler_env: true }) }
-  let(:bummr_gem_path) { File.expand_path("../../", __dir__) }
-  let(:git) { "LANG=en_US git"}
+RSpec.shared_examples "from root folder" do |path|
+  let(:gemfile_path) { "#{path}Gemfile"}
+  let(:rakefile_path) { "#{path}Rakefile"}
+  let(:cd_if_necessary) { path ? "cd #{path} && " : nil }
 
   it "updates outdated gems" do
-    session.create_file "Gemfile", <<~RUBY
-      source "https://rubygems.org"
-      gem "rake", "~> 10.0"
-      gem "bummr", path: "#{bummr_gem_path}"
+    session.create_file gemfile_path, <<~RUBY
+    source "https://rubygems.org"
+    gem "rake", "~> 10.0"
+    gem "bummr", path: "#{bummr_gem_path}"
     RUBY
 
-    session.create_file "Rakefile", <<~RUBY
-      task :default do
-        puts "Hello from the Rakefile"
-      end
+    session.create_file rakefile_path, <<~RUBY
+    task :default do
+      puts "Hello from the Rakefile"
+    end
     RUBY
 
-    expect(session.run("bundle install --retry 3")).
+    expect(session.run("#{cd_if_necessary}bundle install --retry 3")).
       to be_a_success
 
     # Now allow newer versions of Rake to be installed
-    session.run("sed -i.bak 's/, \"~> 10.0\"//' Gemfile")
+    session.run("sed -i.bak 's/, \"~> 10.0\"//' #{gemfile_path}")
 
     session.run("mkdir -p log")
 
@@ -39,7 +39,7 @@ describe "bummr update command" do
     session.run("#{git} checkout -b bummr-branch")
 
     update_result = session.run(
-      "bundle exec bummr update",
+      "#{cd_if_necessary}bundle exec bummr update",
       stdin: "y\ny\ny\n",
       env: { EDITOR: nil, BUMMR_HEADLESS: "true" },
     )
@@ -54,7 +54,22 @@ describe "bummr update command" do
     expect(session.run("#{git} log")).
       to be_a_success.and have_stdout(rake_gem_updated)
 
-    expect(session.run("bundle show")).
+    expect(session.run("#{cd_if_necessary}bundle show")).
       to be_a_success.and have_stdout(/rake\s\(1[1-9]/)
+  end
+end
+
+describe "bummr update command" do
+  let(:session) { JetBlack::Session.new(options: { clean_bundler_env: true }) }
+  let(:bummr_gem_path) { File.expand_path("../../", __dir__) }
+  let(:git) { "LANG=en_US git"}
+
+
+  context "from root folder" do
+    it_behaves_like "from root folder"
+  end
+
+  context "from included folder" do
+    it_behaves_like "from root folder", "code/src/"
   end
 end
